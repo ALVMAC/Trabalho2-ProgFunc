775b59369f6a58f031a8248305ce8d5941282750
root <root@Andr>
2024-11-06 20:07:09 -0300

    Add alguns execs(nao sei se ta funcionando ainda

--- teste2.hs
+++ teste2.hs
@@ -1,12 +1,14 @@
 type Memoria = [(Int, Int)]
+type Acumulador = Int
+type FlagEQZ = Int
 
 
--- executa do endereço 0
+-- executa do endereço 0 ok
 executar :: Memoria -> Memoria
 executar mem = loop mem 0 0 0
     where 
         loop memoria pc acc eqz
-         | opcode == 20 = memoria  -- 20 é o hlt e f
+         | opcode == 20 = memoria  -- 20 é o exechlt e f
          | otherwise = loop memoria' pc' acc' eqz'
          where
             opcode = readMem memoria pc
@@ -14,6 +16,9 @@ executar mem = loop mem 0 0 0
             (memoria', acc', eqz') = executarInstrucao opcode opprox (memoria, acc, eqz)
             pc' = pc + 2            
 
+
+
+-- aqui e pra executar uma função especifica (arrumar a compilação)
 executarInstrucao :: Int -> Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
 executarInstrucao 2 end = execLOD end
 executarInstrucao 4 end = execSTO end
@@ -25,6 +30,21 @@ executarInstrucao 16 end = execSUB end
 executarInstrucao 18 _ = execNOP
 executarInstrucao 20 _ = execHLT
 executarInstrucao _ _ = execNOP
+-- PRECISA FAZER ESSAS FUNÇÔES execLOD execSTO ETC
+-- codigo de dicas
+execLOD ::Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ) 
+execLOD end (mem, acc, eqz) = (mem, readMem mem end, eqz)
+
+execSTO :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
+execJMP ::
+execJMZ ::
+execCPE ::
+execADD ::
+execSUB ::
+execHLT ::
+-- do nosso codigo de dicas
+execNOP :: (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ) 
+execNOP = (mem, acc, eqz) = (mem, acc, eqz)
 
 -- altera a entrada para Memoria que é a lista de tuplas que tinha antes na função da prof.
 readMem :: Memoria -> Int -> Int
@@ -32,3 +52,8 @@ readMem (m:ms) e
     | e == fst m = snd m
     | e /= fst m = readMem ms e
 
+writeMem :: Memoria -> Int -> Int -> Memoria
+writeMem [] end val = [(end, val)]
+writeMem ((end', val'):ms) end val
+    | end == end' = (end, val) : ms
+    | otherwise = (end', val') : writeMem ms end val
