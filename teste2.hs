809ef2cec3c5edd27e82b6fe64a3d779b537797d
root <root@Andr>
2024-11-08 11:27:55 -0300

    ACHO QUE TA FUNCIONANDO, FALTA 1

--- teste2.hs
+++ teste2.hs
@@ -5,21 +5,21 @@ type FlagEQZ = Int
 
 -- executa do endereço 0 ok
 executar :: Memoria -> Memoria
-executar mem = loop mem 0 0 0
+executar mem = loop mem 0 0 0 
     where 
         loop memoria pc acc eqz
-         | opcode == 20 = memoria  -- 20 é o exechlt e f
+         | opcode == 20 = memoria  -- 20 is execHLT
          | otherwise = loop memoria' pc' acc' eqz'
          where
             opcode = readMem memoria pc
-            opprox = readMem memoria (pc+1)
-            (memoria', acc', eqz') = executarInstrucao opcode opprox (memoria, acc, eqz)
-            pc' = pc + 2            
+            opprox = readMem memoria (pc + 1)
+            (memoria', acc', eqz', pc') = executarInstrucao opcode opprox (memoria, acc, eqz, pc + 2)
+       
 
 
 
 -- aqui e pra executar uma função especifica (arrumar a compilação)
-executarInstrucao :: Int -> Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
+executarInstrucao :: Int -> Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
 executarInstrucao 2 end = execLOD end
 executarInstrucao 4 end = execSTO end
 executarInstrucao 6 end = execJMP end
@@ -30,44 +30,41 @@ executarInstrucao 16 end = execSUB end
 executarInstrucao 18 _ = execNOP
 executarInstrucao 20 _ = execHLT
 executarInstrucao _ _ = execNOP
+
+
 -- PRECISA FAZER ESSAS FUNÇÔES execLOD execSTO ETC
 -- codigo de dicas
-execLOD ::Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ) 
-execLOD end (mem, acc, eqz) = (mem, readMem mem end, eqz)
+execLOD :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execLOD end (mem, acc, eqz, pc) = (mem, readMem mem end, eqz, pc)
 
-execSTO :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execSTO end (mem, acc, eqz) = (writeMem mem end acc, acc, eqz)
+execSTO :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execSTO end (mem, acc, eqz, pc) = (writeMem mem end acc, acc, eqz, pc)
 
-execJMP :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execJMP end (mem, acc, eqz) = (mem, acc, eqz)
+execJMP :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execJMP end (mem, acc, eqz, _) = (mem, acc, eqz, end)
 
+execJMZ :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execJMZ end (mem, acc, eqz, pc)
+    | eqz == 1  = (mem, acc, eqz, end)  -- Jump if eqz == 1
+    | otherwise = (mem, acc, eqz, pc)
 
-execJMZ :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execJMZ end (mem, acc, eqz)
-    | eqz == 1  = (mem, acc, eqz)  -- Salta se eqz for 1 (acumulador == 0)
-    | otherwise = (mem, acc, eqz)  -- Caso contrário, não faz nada
+execCPE :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execCPE end (mem, acc, _, pc)
+    | acc == readMem mem end = (mem, acc, 1, pc)  -- Set eqz to 1 if equal
+    | otherwise = (mem, acc, 0, pc)
 
+execADD :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execADD end (mem, acc, eqz, pc) = (mem, acc + readMem mem end, eqz, pc)
 
-execCPE :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execCPE end (mem, acc, _)
-    | acc == readMem mem end = (mem, acc, 1)  -- Se forem iguais, seta eqz para 1
-    | otherwise              = (mem, acc, 0)  -- Caso contrário, seta eqz para 0
+execSUB :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execSUB end (mem, acc, eqz, pc) = (mem, acc - readMem mem end, eqz, pc)
 
+execHLT :: (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execHLT (mem, acc, eqz, pc) = (mem, acc, eqz, pc)  -- Halt execution
 
-execADD :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execADD end (mem, acc, eqz) = (mem, acc + readMem mem end, eqz)
+execNOP :: (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
+execNOP (mem, acc, eqz, pc) = (mem, acc, eqz, pc)  -- No operation
 
-
-execSUB :: Int -> (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execSUB end (mem, acc, eqz) = (mem, acc - readMem mem end, eqz)
-
-
-execHLT :: (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ)
-execHLT (mem, acc, eqz) = (mem, acc, eqz)
-
--- do nosso codigo de dicas
-execNOP :: (Memoria, Acumulador, FlagEQZ) -> (Memoria, Acumulador, FlagEQZ) 
-execNOP (mem, acc, eqz) = (mem, acc, eqz)
 
 -- altera a entrada para Memoria que é a lista de tuplas que tinha antes na função da prof.
 readMem :: Memoria -> Int -> Int
@@ -82,4 +79,3 @@ writeMem [] end val = [(end, val)]
 writeMem ((end', val'):ms) end val
     | end == end' = (end, val) : ms  
     | otherwise = (end', val') : writeMem ms end val
-
