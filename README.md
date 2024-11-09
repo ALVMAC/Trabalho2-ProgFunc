b4c8aadb98c29e57c4b7f713e0f511b9e8983abb
root <root@Andr>
2024-11-09 14:49:44 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Trabalho2-ProgFunc
+A gente tem os seguintes programas definidos
+
+let prog1 = [(0,2),(1,240),(2,14),(3,241),(4,4),(5,251),(6,20),(7,18),(240,8),(241,2),(251,0)]
+let prog2 = [(0,2),(1,240),(2,14),(3,241),(4,16),(5,242),(6,4),(7,251),(8,20),(9,18),(240,28),(241,1),(242,2),(251,0)]
+let prog3 = [(0,2),(1,240),(2,4), (3,244),(4,2),(5,241),(6,4),(7,245),(8,2),(9,245),(10,10),(11,246),(12,8),(13,28),(14,2),(15,251),(16,14),(17,244),(18,4),(19,251),(20,2),(21,245),(22,16),(23,247),(24,4),(25,245),(26,6),(27,8),(28,20),(29,18),(240,2),(241,0),(244,0),(245,0),(246,0),(247,1),(251,0)]
+let prog4 = [(0,2),(1,240),(2,4), (3,251),(4,2),(5,241),(6,4),(7,245),(8,2),(9,245),(10,10),(11,246),(12,8),(13,28),(14,2),(15,251),(16,14),(17,248),(18,4),(19,251),(20,2),(21,245),(22,14),(23,247),(24,4),(25,245),(26,6),(27,8),(28,20),(29,18),(240,1),(241,0),(244,0),(245,0),(246,5),(247,1),(248,2),(251,0)]
+
+onde eles fazer o seguinte:
+Prog1 ele soma dois valores que estão nos endereços 240 e 241 e é guardado posteriormente no 251
+Para executar ele no ghci precisa colocar exatamente o que tem na linha e posteriormente colocar o comando "executar prog1"
+
+Prog2 é o teste 1 do enunciado onde A+B-2 pegando os valores que estão nos endereços 240,241 e o 2 que esta no endereço 242 e aramazenando no 251
+execução: colar o programa no ghci e digitar o comando: executar prog2
+
+prog3 é o teste 2, onde é A*B e como não tem uma função mul ele realiza soma sucessivas enquando um dos valores nao chega a 0
+execução: colar o programa no ghci e digitar o comando: executar prog3
+
+prog4 é o teste 3 onde A = 0; Resp = 1; while(A < 5) { A = A + 1; Resp = Resp + 2; } Então ele incrementa um auxiliar até que seja igual a 5 e quando for ele pula a execução para o final.
+execução: colar o programa no ghci e digitar o comando: executar prog4
+
--- teste2.hs
+++ teste2.hs
@@ -2,13 +2,75 @@ type Memoria = [(Int, Int)]
 type Acumulador = Int
 type FlagEQZ = Int
 
+--0 LOD 240   
+--2 ADD 241   
+--4 STO 251   
+--6 HLT       
+--7 NOP  
+-- 8+4 = 12 precisa 
+prog1 :: Memoria
+prog1 = [(0,2),(1,240),(2,14),(3,241),(4,4),(5,251),(6,20),(7,18),(240,8),(241,4),(251,0)]
+
+--prog2
+--0 LOD 240   
+--2 ADD 241   
+--4 SUB 242   
+--6 STO 251   
+--8 HLT       
+--9 NOP   
+--8+4-2 
+prog2 :: Memoria
+prog2 = [(0,2),(1,240),(2,14),(3,241),(4,16),(5,242),(6,4),(7,251),(8,20),(9,18),(240,8),(241,4),(242,2),(251,0)]
+
+--prog3
+--0 LOD 240   
+--2 STO 244   
+--4 LOD 241   
+--6 STO 245   
+--8 LOD 245   
+--10 CPE 246  
+--12 JMZ 28   
+--14 LOD 251  
+--16 ADD 244  
+--18 STO 251  
+--20 LOD 245  
+--22 SUB 247  
+--24 STO 245  
+--26 JMP 8    
+--28 HLT      
+--29 NOP  
+-- 14 * 4 = 56    
+
+prog3 :: Memoria
+prog3 = [(0,2),(1,240),(2,4), (3,244),(4,2),(5,241),(6,4),(7,245),(8,2),(9,245),(10,10),(11,246),(12,8),(13,28),(14,2),(15,251),(16,14),(17,244),(18,4),(19,251),(20,2),(21,245),(22,16),(23,247),(24,4),(25,245),(26,6),(27,8),(28,20),(29,18),(240,14),(241,4),(244,0),(245,0),(246,0),(247,1),(251,0)]
+
+--prog4
+--0 LOD 240   
+--2 STO 251   
+--4 LOD 241   
+--6 STO 245   
+--8 LOD 245   
+--10 CPE 246  
+--12 JMZ 28   
+--14 LOD 251  
+--16 ADD 248  
+--18 STO 251  
+--20 LOD 245  
+--22 ADD 247  
+--24 STO 245  
+--26 JMP 8    
+--28 HLT      
+--29 NOP      
+--A = 0; Resp = 1; while(A < 5) { A = A + 1; Resp = Resp + 2;}
+prog4 :: Memoria
+prog4 = [(0,2),(1,240),(2,4), (3,251),(4,2),(5,241),(6,4),(7,245),(8,2),(9,245),(10,10),(11,246),(12,8),(13,28),(14,2),(15,251),(16,14),(17,248),(18,4),(19,251),(20,2),(21,245),(22,14),(23,247),(24,4),(25,245),(26,6),(27,8),(28,20),(29,18),(240,1),(241,0),(244,0),(245,0),(246,5),(247,1),(248,2),(251,0)]
 
 -- executa do endereço 0 ok
 executar :: Memoria -> Memoria
 executar mem = loop mem 0 0 0 
     where 
         loop memoria pc acc eqz
-         | opcode == 20 = memoria  -- 20 is execHLT
+         | opcode == 20 = memoria  -- finaliza a brincadeira
          | otherwise = loop memoria' pc' acc' eqz'
          where
             opcode = readMem memoria pc
@@ -18,7 +80,7 @@ executar mem = loop mem 0 0 0
 
 
 
--- aqui e pra executar uma função especifica (arrumar a compilação)
+-- aqui e pra executar uma função especifica eu adicionei um inteiro pra levar o pc junto pq sem ele não tava conseguindo fazer o jmp nem njmz
 executarInstrucao :: Int -> Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
 executarInstrucao 2 end = execLOD end
 executarInstrucao 4 end = execSTO end
@@ -32,8 +94,8 @@ executarInstrucao 20 _ = execHLT
 executarInstrucao _ _ = execNOP
 
 
--- PRECISA FAZER ESSAS FUNÇÔES execLOD execSTO ETC
--- codigo de dicas
+
+-- codigo de dicas, mas adicionando o pc que vai junto
 execLOD :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
 execLOD end (mem, acc, eqz, pc) = (mem, readMem mem end, eqz, pc)
 
@@ -45,12 +107,12 @@ execJMP end (mem, acc, eqz, _) = (mem, acc, eqz, end)
 
 execJMZ :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
 execJMZ end (mem, acc, eqz, pc)
-    | eqz == 1  = (mem, acc, eqz, end)  -- Jump if eqz == 1
+    | eqz == 1  = (mem, acc, eqz, end)  -- oula se é 1 
     | otherwise = (mem, acc, eqz, pc)
 
 execCPE :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
 execCPE end (mem, acc, _, pc)
-    | acc == readMem mem end = (mem, acc, 1, pc)  -- Set eqz to 1 if equal
+    | acc == readMem mem end = (mem, acc, 1, pc)  -- coloca a flag em 1 
     | otherwise = (mem, acc, 0, pc)
 
 execADD :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
@@ -60,10 +122,10 @@ execSUB :: Int -> (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, F
 execSUB end (mem, acc, eqz, pc) = (mem, acc - readMem mem end, eqz, pc)
 
 execHLT :: (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
-execHLT (mem, acc, eqz, pc) = (mem, acc, eqz, pc)  -- Halt execution
+execHLT (mem, acc, eqz, pc) = (mem, acc, eqz, pc)  
 
 execNOP :: (Memoria, Acumulador, FlagEQZ, Int) -> (Memoria, Acumulador, FlagEQZ, Int)
-execNOP (mem, acc, eqz, pc) = (mem, acc, eqz, pc)  -- No operation
+execNOP (mem, acc, eqz, pc) = (mem, acc, eqz, pc)  
 
 
 -- altera a entrada para Memoria que é a lista de tuplas que tinha antes na função da prof.
@@ -74,7 +136,7 @@ readMem (m:ms) e
 
 writeMem :: Memoria -> Int -> Int -> Memoria
 writeMem _ end _ | end >= 256 = error "Erro: endereço precisa estar entre 0 e 255"
-writeMem _ _ val | val > 255 = error "Erro: valor precisa estar entre 0 e 255 (8 bits) parceiro"
+writeMem _ _ val | val > 255 = error "Erro: valor precisa estar entre 0 e 255 so tem 8bitss"
 writeMem [] end val = [(end, val)] 
 writeMem ((end', val'):ms) end val
     | end == end' = (end, val) : ms  
